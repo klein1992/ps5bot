df9f67d38ad5cd7d6e53e5b13255529cea2550d7
klein1992 <ejw695@gmail.com>
2021-03-13 14:51:51 -0500

    Add boto3 library, publish to aws topic when ps5 is ready to order

--- bestbuy/bestbuy.py
+++ bestbuy/bestbuy.py
@@ -1,14 +1,29 @@
+import os
+
+import boto3
 import requests
 from requests_html import HTMLSession
 
-searchUrl = (
-    "https://www.bestbuy.com/site/sony-playstation-5-console/6426149.p?skuId=6426149"
-)
+awsDefaultRegion = os.getenv('AWS_DEFAULT_REGION')
+awsTopicARN = os.getenv('AWS_TOPIC_ARN')
+searchUrl = os.getenv('SEARCH_URL')
+
+textMessage='PS5 Is Ready to Order Hurry!'
+textMessageSubject='From AWS PS5 Bot'
 
 try:
     session = HTMLSession()
     isOrderable = session.get(searchUrl).html.find("button", containing="Add to Cart")
-    print("Ready to Order" if (isOrderable) else "The ps5 is sold out right now")
+    if isOrderable:
+        print("Ready to Order")
+        client = boto3.client('sns', awsDefaultRegion)
+        response = client.publish(
+        TopicArn=awsTopicARN,
+        Message=textMessage,
+        Subject=textMessageSubject
+        )
+    else:
+        print("The ps5 is sold out right now")
 
 except requests.exceptions.RequestException as e:
     print(e)
