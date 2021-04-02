ee584f8458ff04c77321063fc74ba2acdac56e1a
klein1992 <ejw695@gmail.com>
2021-04-02 02:52:12 -0400

    Implent full checkout functionality

--- bestbuy/checkout/bestbuy-checkout.py
+++ bestbuy/checkout/bestbuy-checkout.py
@@ -1,61 +1,47 @@
-import requests
+import requests, time, os
 from chromedriver_py import binary_path
 from requests_html import HTMLSession
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 
-baseUrl = "https://bestbuy.com/"
+baseUrl = "https://www.bestbuy.com/site/demons-souls-standard-edition-playstation-5/6430152.p?skuId=6430152"
 
-driver = webdriver.Chrome(executable_path=binary_path)
+# "https://www.bestbuy.com/site/sony-playstation-5-console/6426149.p?skuId=6426149"
+
+chrome_options = Options()
+# chrome_options.add_argument('--no-sandbox')
+# chrome_options.add_argument('--window-size=1420,1080')
+# chrome_options.add_argument('--headless')
+# chrome_options.add_argument('--disable-gpu')
+
+# chrome_options.add_argument('--no-proxy-server')
+# chrome_options.add_argument("--proxy-server='direct://'")
+# chrome_options.add_argument("--proxy-bypass-list=*")
+# chrome_options.add_argument('--blink-settings=imagesEnabled=false')
+
+start_time = time.time()
+driver = webdriver.Chrome(executable_path=binary_path, options=chrome_options)
 driver.get(baseUrl)
 
-modal_close_button = WebDriverWait(driver, 5).until(
-    EC.element_to_be_clickable(
-        (By.CSS_SELECTOR, ".widgets-view-email-modal .c-modal-grid .c-close-icon")
-    )
-)
-
-print("Closing modal...")
-modal_close_button.click()
-search_bar = WebDriverWait(driver, 5).until(
-    EC.element_to_be_clickable((By.XPATH, "//*[@placeholder='Search Best Buy']"))
-)
-
-print("typing into search bar...")
-search_bar.click()
-search_bar.send_keys("demon souls")
-search_button = WebDriverWait(driver, 5).until(
-    EC.element_to_be_clickable((By.XPATH, "//button[@title='submit search']"))
-)
-
-print("clicking enter for search...")
-search_button.click()
-
-add_to_cart_button = driver.find_elements_by_class_name("add-to-cart-button")[0]
-
-data_sku_id = add_to_cart_button.get_attribute('data-sku-id')
-
 print("clicking add to cart...")
-add_to_cart_button_clickable = WebDriverWait(driver, 10).until(
+add_to_cart_button = WebDriverWait(driver, 10).until(
     EC.element_to_be_clickable(
-        (By.XPATH, f"//button[@data-sku-id='{data_sku_id}']")
+        (By.CLASS_NAME, "add-to-cart-button")
     )
 )
 
-add_to_cart_button_clickable.click()
+add_to_cart_button.click()
 
 print("going to cart...")
 goto_cart_button = WebDriverWait(driver, 10).until(
     EC.element_to_be_clickable(
-        (By.XPATH, "//*[@href='/cart']")
+        (By.XPATH, "//a[@href='/cart']")
     )
 )
-print(goto_cart_button.text)
 goto_cart_button.click()
-
-
 
 print("checking out...")
 checkout_button = WebDriverWait(driver, 5).until(
@@ -83,7 +69,7 @@ input_email = WebDriverWait(driver, 5).until(
 )
 
 input_email.click()
-input_email.send_keys("")
+input_email.send_keys(os.getenv('EMAIL'))
 
 input_password = WebDriverWait(driver, 5).until(
     EC.element_to_be_clickable(
@@ -92,7 +78,7 @@ input_password = WebDriverWait(driver, 5).until(
 )
 
 input_password.click()
-input_password.send_keys("")
+input_password.send_keys(os.getenv('PASSWORD'))
 
 print("Logging in...")
 sign_in_button = WebDriverWait(driver, 5).until(
@@ -112,7 +98,7 @@ cvv_input = WebDriverWait(driver, 5).until(
 )
 
 cvv_input.click()
-cvv_input.send_keys("")
+cvv_input.send_keys(os.getenv('CVV'))
 
 print("Placing Order...")
 place_order_button = WebDriverWait(driver, 5).until(
@@ -122,15 +108,3 @@ place_order_button = WebDriverWait(driver, 5).until(
 )
 
 place_order_button.click()
-
-
-try:
-    session = HTMLSession()
-    search_response = session.get(searchUrl)
-    selection = "h4 > a"
-    ps5_link = search_response.html.find(selection, first=True).absolute_links.pop()
-    print(ps5_link)
-    soldOutButton = session.get(ps5_link).html.find("button", containing="sold out")
-    print("Ready to Order" if (not soldOutButton) else "The ps5 is sold out right now")
-except requests.exceptions.RequestException as e:
-    print(e)
